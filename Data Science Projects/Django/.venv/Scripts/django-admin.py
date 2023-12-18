0373f6b1a4956b480e07dac18d11b5eb8fc76699
Walter Nathaniel <wltr.nathaniel@gmail.com>
2023-12-18 15:19:48 +0300

    Created Django project and app

---
+++ Data Science Projects/Django/.venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Walter Owando\Documents\GitHub\Data-Science-Projects\Data Science Projects\Django\.venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
